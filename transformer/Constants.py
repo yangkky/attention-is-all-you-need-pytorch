a1e74beded29b6503e7f6b0386f5826b0905afa6
yangkky <seinchin@gmail.com>
2017-10-16 17:07:58 -0700

    Change constants.

--- transformer/Constants.py
+++ transformer/Constants.py
@@ -1,10 +1,8 @@
 
 PAD = 0
-UNK = 1
-BOS = 2
-EOS = 3
+BOS = 1
+EOS = 2
 
-PAD_WORD = '<blank>'
-UNK_WORD = '<unk>'
-BOS_WORD = '<s>'
-EOS_WORD = '</s>'
+PAD_WORD = ' '
+BOS_WORD = '$'
+EOS_WORD = '.'
